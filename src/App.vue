0065eb1db04ef6364975b82c0fc4ee14579aca45
Admin <anatol1812@gmail.com>
2021-11-05 00:10:09 +0200

    first part of tutorial

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div id="app">
+   <h1>Hello dude</h1>
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+   
   }
 }
 </script>
